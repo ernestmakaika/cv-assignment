f4a085552166d2bbd1d0ae128b4f71065830bf0b
emakaika <ernestmakaika@gmail.com>
2020-09-25 14:39:12 +0200

    Did validation for the form.

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
-function name(params) {
+function cvAssignment () {
+    console.log ("cvAssignment");
+    const indexForm = document.getElementById("form");
+    indexForm.addEventListener("submit",indexValidation)
+}
+
+window.onload = cvAssignment;
+
+function indexValidation (element, event){
+    console.log(element);
+
+    const firstName = document.getElementById("firstname");
+    if (firstName.value.length==0){
+        window.alert("Please, enter your first name.")
+    }
+    else {
+        localStorage.setItem("firstname", firstName.value);
+        const cvFirstname = localStorage.getItem("firstname")
+    };
+
+    const lastName = document.getElementById("lastname");
+    if (lastname.value.length==0){
+    window.alert("Please, enter your last name.")
+    }
+    else {
+        localStorage.setItem("lastname", lastName)
+        const cvLastname = localStorage.getItem("lastname")
+
+    };
+
+    const address = document.getElementById("address");
+    if (address.value.length==0){
+        window.alert("Please, enter your address.")
+    }
+    else {
+        localStorage.setItem("address", address);
+        const cvAddress = localStorage.getElementById.getItem("address")
+    };
+
+    
+    const phone = document.getElementById("phone")
+    if (phone.value.length==0){
+        window.alert("Please, enter your phone number.")
+    }
+    else {
+        localStorage.setItem("phone", phone)
+        const cvPhone = localStorage.getElementById.getElementById("phone")
+    };
     
     const email = document.getElementById("email");
     if (email.value.length==0){
-        window.alert("Please, enter your email address.")
-}
+        window.alert("Please, enter your email.")
+    }
     else{
-        const cvEmail = localStorage.getElementById.getItem("email")
-    }}
-    ;
+        localStorage.setItem("email", email)
+        const cvEmail = localStorage.getElementById.getElementById("email")
+    };
+}
