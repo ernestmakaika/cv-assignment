2c32f90a4271d9d43165897a1747bd6c24c65603
emakaika <ernestmakaika@gmail.com>
2020-09-25 14:32:46 +0200

    Did some form validation.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function name(params) {
     
+    const email = document.getElementById("email");
+    if (email.value.length==0){
+        window.alert("Please, enter your email address.")
 }
+    else{
+        const cvEmail = localStorage.getElementById.getItem("email")
+    }}
+    ;
