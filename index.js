b96ebff14b9e59645a23520763eba01cb18b5548
emakaika <ernestmakaika@gmail.com>
2020-09-26 02:24:39 +0200

    Finished the styling and validation of the form and CV, as well as the retrieval of data from the local storage.

--- index.js
+++ index.js
@@ -9,49 +9,49 @@ window.onload = cvAssignment;
 function indexValidation (element, event){
     console.log(element);
 
-    const firstName = document.getElementById("firstname");
-    if (firstName.value.length==0){
+    const firstName = document.getElementById("firstname").value;
+    if (firstName.length==0){
         window.alert("Please, enter your first name.")
     }
     else {
         localStorage.setItem("firstname", firstName);
-        const cvFirstname = localStorage.getItem("firstname")
     };
 
-    const lastName = document.getElementById("lastname");
-    if (lastname.value.length==0){
+    const lastName = document.getElementById("lastname").value;
+    if (lastName.length==0){
     window.alert("Please, enter your last name.")
     }
     else {
         localStorage.setItem("lastname", lastName)
-        const cvLastname = localStorage.getItem("lastname")
+      
 
     };
 
-    const address = document.getElementById("address");
-    if (address.value.length==0){
+    const address = document.getElementById("address").value;
+    if (address.length==0){
         window.alert("Please, enter your address.")
     }
     else {
         localStorage.setItem("address", address);
-        const cvAddress = localStorage.getItem("address")
+        
     };
 
-    const phone = document.getElementById("phone");
-    if (phone.value.length==0){
+    const phone = document.getElementById("phone").value;
+    if (phone.length==0){
         window.alert("Please, enter your phone number.")
     }
     else {
         localStorage.setItem("phone", phone)
-        const cvPhone = localStorage.getItem("phone")
+        // const cvPhone = localStorage.getItem("phone")
     };
     
-    const email = document.getElementById("email");
-    if (email.value.length==0){
+    const email = document.getElementById("email").value;
+    if (email.length==0){
         window.alert("Please, enter your email.")
     }
     else{
         localStorage.setItem("email", email)
-        const cvEmail = localStorage.getItem("email")
+        // const cvEmail = localStorage.getItem("email")
     };
+
 }
